442c2a2c709d5ad4499f842ddbf15ee1498d9488
Namazouchan <namazou.challenger@gmail.com>
2024-01-22 22:29:48 +0900

    post_articles_fin

---
+++ db/migrate/20240122130649_create_articles.rb
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration[7.1]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :description
+      t.text :body
+      t.string :slug
+      t.text :tagList
+
+      t.timestamps
+    end
+  end
+end
